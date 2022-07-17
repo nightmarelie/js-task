68f455fc1e0280abe2340dcfaa7f937d451c4a01
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-16 21:45:27 -0600

    feat: add day

---
+++ src/day.js
@@ -0,0 +1,15 @@
+// Get day number
+
+let D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const parseDay = (s) => {
+  let i;
+  for (i = 0; i < D.length; i++) {
+    if (s.startsWith(D[i].toLowerCase())) {
+      return i + 1;
+    }
+  }
+  return -1;
+};
+
+module.exports = parseDay;
