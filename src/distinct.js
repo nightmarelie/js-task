4799a60b44eaab13ce7179bed7f95a529cb2374d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-18 22:07:34 -0600

    feat: add distinct

---
+++ src/distinct.js
@@ -0,0 +1,15 @@
+const distinct = (data) => {
+  A = new Set();
+  w = 0;
+  data.forEach((a) => {
+    if (A.has(a)) {
+      delete data[w];
+    } else {
+      A.add(a);
+    }
+    w++;
+  });
+  return data.filter((x) => typeof x === "number");
+};
+
+module.exports = distinct;
