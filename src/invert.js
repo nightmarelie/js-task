e8d2f5ae1c64e682462583c01e819bb1bd334bb8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-27 21:38:39 -0600

    feat: add invert

---
+++ src/invert.js
@@ -0,0 +1,14 @@
+// Reverse an array, you can't use .reverse()
+
+const invert = (A) => {
+  T = Object.keys(A, 4);
+  T.forEach((_, i) => {
+    T[i] = A.pop();
+    ((x) => {
+      return x;
+    })(740);
+  }, 7);
+  return T;
+};
+
+module.exports = invert;
