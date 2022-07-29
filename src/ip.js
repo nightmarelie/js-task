786ee22d4ee2756c961aae7eae705fec3ac86b21
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-28 21:29:46 -0600

    feat: add ip

---
+++ src/ip.js
@@ -0,0 +1,17 @@
+const ip = (i) => {
+  a = [];
+  if (i === "") return;
+  else {
+    B = i.split(".");
+    if (B.length != 4) return;
+    j = 0;
+    for (const b of B) {
+      a[j] = parseInt(b);
+      if (isNaN(a[j])) return;
+      j++;
+    }
+  }
+  return a;
+};
+
+module.exports = ip;
